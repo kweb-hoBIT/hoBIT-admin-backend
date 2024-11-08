ae865ccb53a77c032ccd4fce95f8accbe2b8134c
BaeHyeonJik <bhj4060@gmail.com>
2024-11-08 20:08:00 +0900

    add api

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import express from "express";
 import cors from "cors";
 import { createTable } from '../config/createTable';
 
+import authRoutes from "./routes/api/auth";
+import usersRoutes from "./routes/api/users";
+import faqsRoutes from "./routes/api/faqs";
+import logsRoutes from "./routes/api/logs";
+import translateRoutes from "./routes/api/translate";
+
 const app = express();
 app.use(cors());
 
@@ -10,11 +16,12 @@ app.use(cors());
   await createTable();
 })();
 
-
 // Express configuration
 app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 // @route   GET /
 // @desc    Test Base API
@@ -27,5 +34,11 @@ const port = app.get("port");
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
 );
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/faqs", faqsRoutes);
+app.use("/api/logs", logsRoutes);
+app.use("/api/translate", translateRoutes);
 
 export default server;

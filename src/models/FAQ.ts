7a037e101e9c9bc858edf112f7ee163abc2b8556
BaeHyeonJik <bhj4060@gmail.com>
2024-11-12 15:57:30 +0900

    edit : model

--- src/models/FAQ.ts
+++ src/models/FAQ.ts
@@ -6,8 +6,8 @@ export type TFAQ = {
   subcategory_en: string;
   question_ko: string;
   question_en: string;
-  answer_ko: string;
-  answer_en: string;
+  answer_ko: Record<string, any>;
+  answer_en: Record<string, any>; 
   manager: string;
   created_by: number | null;
   updated_by: number | null;

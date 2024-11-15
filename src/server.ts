d62ea6f20a1f0504231e99cfd9a4ff766548e965
BaeHyeonJik <bhj4060@gmail.com>
2024-11-15 11:14:00 +0900

    REFACTOR: change port to 3000

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(cors());
 })();
 
 // Express configuration
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 

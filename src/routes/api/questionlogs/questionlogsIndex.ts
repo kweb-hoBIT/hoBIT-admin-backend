0d919ecc215e64af91beea80b31043d680f4ca2a
BaeHyeonJik <bhj4060@gmail.com>
2025-01-03 16:22:17 +0900

    FEAT: specific analyze

--- src/routes/api/questionlogs/questionlogsIndex.ts
+++ src/routes/api/questionlogs/questionlogsIndex.ts
@@ -4,6 +4,10 @@ import getAllQuestionLogsRoutes from './getAllQuestionLogs';
 import QuestionLogsFrequencyRoutes from './questionLogsFrequency';
 import QuestionLogsFeedbackRoutes from './questionLogsFeedback';
 import QuestionLogsLanguageRoutes from './questionLogsLanguage';
+import QuestionLogFrequencyRoutes from './questionLogFrequency';
+import QuestionLogFeedbackRoutes from './questionLogFeedback';
+import QuestionLogLanguageRoutes from './questionLogLanguage';
+
 
 const router = express.Router();
 
@@ -11,6 +15,9 @@ router.use('/', getAllQuestionLogsRoutes);
 router.use('/', QuestionLogsFrequencyRoutes);
 router.use('/', QuestionLogsFeedbackRoutes);
 router.use('/', QuestionLogsLanguageRoutes);
+router.use('/', QuestionLogFrequencyRoutes);
+router.use('/', QuestionLogFeedbackRoutes);
+router.use('/', QuestionLogLanguageRoutes);
 
 
 export default router;

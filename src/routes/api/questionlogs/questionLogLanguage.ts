40464587d902e414c46aef9d602634fe9c14e526
BaeHyeonJik <bhj4060@gmail.com>
2025-01-03 16:22:17 +0900

    FEAT: specific analyze

--- src/routes/api/questionlogs/questionLogLanguage.ts
+++ src/routes/api/questionlogs/questionLogLanguage.ts
@@ -1,28 +1,29 @@
 import express, { Request, Response } from "express";
 import { Pool } from "../../../../config/connectDB";
 import { PoolConnection, RowDataPacket } from "mysql2/promise";
-import { EntireFrequencyRequest, EntireFrequencyResponse } from "../../../types/questionLog";
+import { SpecificLanguageRequest, SpecificLanguageResponse } from "../../../types/questionLog";
 import _ from "lodash";
 import moment from "moment";
 
 const router = express.Router();
 
-// @route   Get api/questionlogs/frequency
-// @desc    Get frequency of question logs
+// @route   Get api/questionlogs/language/:faq_id
+// @desc    Get frequency of question log by language
 // @access  Private
-router.get("/frequency", async (req: Request, res: Response) => {
+router.get("/language/:faq_id", async (req: Request<SpecificLanguageRequest['params']>, res: Response) => {
   const connection: PoolConnection = await Pool.getConnection();
-  const { startDate, endDate, period, sortOrder, limit } = req.query as EntireFrequencyRequest['query'];
-  console.log(req.query);
+  const { faq_id } = req.params;
+  const { startDate, endDate, period} = req.query as SpecificLanguageRequest['query'];
+  console.log(req.params, req.query);
 
   try {
-    const sortorder = parseInt(sortOrder) ? "DESC" : "ASC";
     const start_date = moment(startDate).format("YYYY-MM-DD");
     const end_date = moment(endDate).format("YYYY-MM-DD");
 
     const intervalType =
       period === "day" ? "1 DAY" : period === "week" ? "1 WEEK" : "1 MONTH";
-    const [dateFrequncy] = await connection.execute<RowDataPacket[]>(
+
+    const [languageFrequency] = await Pool.execute<RowDataPacket[]>(
       `WITH RECURSIVE DateRange AS (
         SELECT ? AS date
         UNION ALL
@@ -35,42 +36,39 @@ router.get("/frequency", async (req: Request, res: Response) => {
         faqs.question_ko,
         DATE_FORMAT(CONVERT_TZ(DateRange.date, '+00:00', '+09:00'), '%Y-%m-%d') AS startDate,
         DATE_FORMAT(CONVERT_TZ(DateRange.date + INTERVAL ${intervalType} - INTERVAL 1 DAY, '+00:00', '+09:00'), '%Y-%m-%d') AS endDate,
-        COALESCE(COUNT(question_logs.faq_id), 0) AS count
+        COALESCE(SUM(CASE WHEN language = 'ko' THEN 1 ELSE 0 END), 0) AS frequency_ko,
+        COALESCE(SUM(CASE WHEN language = 'en' THEN 1 ELSE 0 END), 0) AS frequency_en
       FROM hobit.faqs
       CROSS JOIN DateRange
       LEFT OUTER JOIN hobit.question_logs 
         ON faqs.id = question_logs.faq_id 
         AND DATE(CONVERT_TZ(question_logs.created_at, '+00:00', '+09:00')) >= DATE_FORMAT(CONVERT_TZ(DateRange.date, '+00:00', '+09:00'), '%Y-%m-%d')
         AND DATE(CONVERT_TZ(question_logs.created_at, '+00:00', '+09:00')) < DATE_FORMAT(CONVERT_TZ(DateRange.date + INTERVAL ${intervalType}, '+00:00', '+09:00'), '%Y-%m-%d')
-      WHERE DateRange.date BETWEEN ? AND ?
+      WHERE faqs.id = ? AND DateRange.date BETWEEN ? AND ?
       GROUP BY DateRange.date, faqs.id
-      ORDER BY DateRange.date, count ${sortorder}, faqs.id;`,
-      [start_date, end_date, start_date, end_date]
+      ORDER BY DateRange.date;`,
+      [start_date, end_date, faq_id, start_date, end_date]
     );
 
-    const groupbyDate = _.groupBy(dateFrequncy, "startDate");
-    const logData: EntireFrequencyResponse['data']['logData'] = {
+   
+    const groupbyDate = _.groupBy(languageFrequency, "startDate");
+    const logData: SpecificLanguageResponse['data']['logData'] = {
       startDate: "",
       endDate: "",
       groupData: [],
     };
-    const groupData : EntireFrequencyResponse['data']['logData']['groupData'] = [];
+
+    const groupData : SpecificLanguageResponse['data']['logData']['groupData'] = [];
     for (const date in groupbyDate) {
       const startDate = date;
       const endDate = groupbyDate[date][0].endDate;
 
-      let rank = 1;
-      const data : EntireFrequencyResponse['data']['logData']['groupData'][0]['data'] = [];
-      for (const item of groupbyDate[date]) {
-        data.push({
-          rank: rank,
-          faq_id: item.faq_id,
-          question_ko: item.question_ko,
-          count: item.count,
-        });
-        if (rank === Number(limit)) break;
-        rank++;
-      }
+      const data : SpecificLanguageResponse['data']['logData']['groupData'][0]['data'] = {
+        faq_id: groupbyDate[date][0].faq_id,
+        question_ko: groupbyDate[date][0].question_ko,
+        ko_frequency: groupbyDate[date][0].frequency_ko,
+        en_frequency: groupbyDate[date][0].frequency_en,
+      };
       groupData.push({
         startDate: startDate,
         endDate: endDate,
@@ -81,9 +79,9 @@ router.get("/frequency", async (req: Request, res: Response) => {
     logData.endDate = end_date;
     logData.groupData = groupData;
 
-    const response : EntireFrequencyResponse = {
+    const response = {
       statusCode: 200,
-      message: "Question logs frequency retrieved successfully",
+      message: "Question log language frequency retrieved successfully",
       data : {
         logData
       }
@@ -92,10 +90,9 @@ router.get("/frequency", async (req: Request, res: Response) => {
     res.status(200).json(response);
   } catch (err: any) {
     const response = {
-      statusCode : 500,
-      message : err.message,
+      statusCode: 500,
+      message: err.message,
     }
-    console.log(response);
     res.status(500).json(response);
   } finally {
     connection.release();

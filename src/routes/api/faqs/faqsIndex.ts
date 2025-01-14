da0e398eb1bc0617ffeacdeed65b3e7326e8fb7f
BaeHyeonJik <bhj4060@gmail.com>
2025-01-14 14:54:43 +0900

    REFACTOR: 라우팅 순서 변경

--- src/routes/api/faqs/faqsIndex.ts
+++ src/routes/api/faqs/faqsIndex.ts
@@ -1,20 +1,22 @@
 import express from 'express';
 import createFAQRoutes from './createFAQ';
+import getAllFAQCategoryRoutes from './getAllFAQCategory';
 import getAllFAQsRotues from './getAllFAQs';
 import getFAQRoutes from './getFAQ';
 import deleteFAQRoutes from './deleteFAQ';
 import updateFAQRoutes from './updateFAQ';
 import createRelatedFAQRoutes from './createRelatedFAQ';
-import getAllFAQCategoryRoutes from './getAllFAQCategory';
+
 
 const router = express.Router();
 
 router.use('/', createFAQRoutes);
+router.use('/', getAllFAQCategoryRoutes);
 router.use('/', getAllFAQsRotues);
 router.use('/', getFAQRoutes);
 router.use('/', deleteFAQRoutes);
 router.use('/', updateFAQRoutes);
 router.use('/', createRelatedFAQRoutes);
-router.use('/', getAllFAQCategoryRoutes);
+
 
 export default router;
--- src/routes/api/seniorfaqs/seniorfaqsIndex.ts
+++ src/routes/api/seniorfaqs/seniorfaqsIndex.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import createSeniorFAQRoutes from './createSeniorFAQ';
+import getAllSeniorFAQCategoryRoutes from './getAllSeniorFAQCategory';
 import getAllSeniorFAQsRoutes from './getAllSeniorFAQs';
 import getSeniorFAQRoutes from './getSeniorFAQ';
 import deleteSeniorFAQRoutes from './deleteSeniorFAQ';
 import updateSeniorFAQRoutes from './updateSeniorFAQ';
-import getAllSeniorFAQCategoryRoutes from './getAllSeniorFAQCategory';
+
 
 const router = express.Router();
 
 router.use('/', createSeniorFAQRoutes);
+router.use('/', getAllSeniorFAQCategoryRoutes);
 router.use('/', getAllSeniorFAQsRoutes);
 router.use('/', getSeniorFAQRoutes);
 router.use('/', deleteSeniorFAQRoutes);
 router.use('/', updateSeniorFAQRoutes);
-router.use('/', getAllSeniorFAQCategoryRoutes);
 
 export default router;

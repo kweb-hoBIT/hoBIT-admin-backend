d58ba6d052215e62905d7f01f8ba1fa362ec71b4
BaeHyeonJik <bhj4060@gmail.com>
2025-01-16 12:58:12 +0900

    test

--- src/routes/api/faqs/faqsIndex.ts
+++ src/routes/api/faqs/faqsIndex.ts
@@ -5,7 +5,7 @@ import getAllFAQsRotues from './getAllFAQs';
 import getFAQRoutes from './getFAQ';
 import deleteFAQRoutes from './deleteFAQ';
 import updateFAQRoutes from './updateFAQ';
-import createRelatedFAQRoutes from './createRelatedFAQ';
+import createRelatedFAQRoutes from './hi';
 
 
 const router = express.Router();

ba710ee0303217460d45afe1bd50b2224f97f9c5
BaeHyeonJik <bhj4060@gmail.com>
2025-01-16 13:03:36 +0900

    test

--- src/routes/api/faqs/faqsIndex.ts
+++ src/routes/api/faqs/faqsIndex.ts
@@ -5,7 +5,7 @@ import getAllFAQsRotues from './getAllFAQs';
 import getFAQRoutes from './getFAQ';
 import deleteFAQRoutes from './deleteFAQ';
 import updateFAQRoutes from './updateFAQ';
-import createRelatedFAQRoutes from './createrelatedFAQ';
+import createRelateFAQRoutes from './createRelateFAQ';
 
 
 const router = express.Router();
@@ -16,7 +16,7 @@ router.use('/', getAllFAQsRotues);
 router.use('/', getFAQRoutes);
 router.use('/', deleteFAQRoutes);
 router.use('/', updateFAQRoutes);
-router.use('/', createRelatedFAQRoutes);
+router.use('/', createRelateFAQRoutes);
 
 
 export default router;

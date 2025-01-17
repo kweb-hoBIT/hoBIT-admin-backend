2838cc8a4d6e30352990b47e62a9707e92b38668
BaeHyeonJik <bhj4060@gmail.com>
2025-01-17 23:31:29 +0900

    FIX: gpt fetch

--- src/routes/api/faqs/updateFAQ.ts
+++ src/routes/api/faqs/updateFAQ.ts
@@ -78,7 +78,7 @@ router.put("/:faq_id", async (req: Request<{ faq_id: UpdateFAQRequest['params'] 
       manager: manager
     }
 
-    if(prev_faq.answer_ko !== new_faq.answer_ko) {
+    if(prev_faq.question_ko !== new_faq.question_en) {
       const gptbody = {
         faq_id: Number(faq_id),
         question: question_ko,

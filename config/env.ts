13604fc06a48803039a047339c774b19811c125a
BAE HYEONJIK <bhj4060@gmail.com>
2025-02-17 23:12:04 +0900

    FEAT: cors 업데이트

--- config/env.ts
+++ config/env.ts
@@ -6,7 +6,8 @@ dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 interface Env {
   PORT: string;
   API_URL: string;
-  CLIENT_URL: string;
+  CLIENT_URL1: string;
+  CLIENT_URL2: string;
   TIMEZONE: string;
   DB_HOST: string;
   DB_USER: string;
@@ -23,7 +24,8 @@ function getEnv(): Env {
   const env = {
     PORT: process.env.PORT,
     API_URL: process.env.API_URL,
-    CLIENT_URL: process.env.CLIENT_URL,
+    CLIENT_URL1: process.env.CLIENT_URL1,
+    CLIENT_URL2: process.env.CLIENT_URL2,
     TIMEZONE: process.env.TIMEZONE,
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,

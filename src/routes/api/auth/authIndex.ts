3c48ea315dffd1ef95dbf3c64390053fb205c5f6
BAE HYEONJIK <bhj4060@gmail.com>
2025-02-17 23:56:50 +0900

    FEAT: Logout Api 추가

--- src/routes/api/auth/authIndex.ts
+++ src/routes/api/auth/authIndex.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 import authRoutes from './auth';
 import loginRoutes from './login';
+import logoutRoutes from './logout';
 import tokenRefreshRoutes from './tokenRefresh';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/auth', loginRoutes);
+router.use('/auth', logoutRoutes);
 router.use('/auth', tokenRefreshRoutes);
 
 export default router;

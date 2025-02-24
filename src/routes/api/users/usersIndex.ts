efc49f2bd8d858172a85db4c8454bf7c5bf2e5f3
BAE HYEONJIK <bhj4060@gmail.com>
2025-02-24 17:03:37 +0900

    FEAT: 비밀번호 재설정 api 추가

--- src/routes/api/users/usersIndex.ts
+++ src/routes/api/users/usersIndex.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 
 import signupRoutes from './signup';
-import deleteAccount from './deleteAccount';
+import deleteAccountRoutes from './deleteAccount';
+import findUserRoutes from './findUser';
+import updatePasswordRoutes from './updatePassword';
 
 const router = express.Router();
 
 router.use('/users', signupRoutes);
-router.use('/users', deleteAccount);
+router.use('/users', deleteAccountRoutes);
+router.use('/users', findUserRoutes);
+router.use('/users', updatePasswordRoutes);
 
 export default router;

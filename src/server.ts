6a366127ee2dca092cf0342fb783f8b58d968f5b
BAE HYEONJIK <bhj4060@gmail.com>
2025-02-25 12:16:59 +0900

    FIX: vercel cors 주소 수정

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const corsOptions = {
     env.CLIENT_URL1,
     env.CLIENT_URL2,
     "https://admin.hobit.kr",
-    /^https:\/\/.*\.vercel\.app$/
+    "https://hobit-admin-frontend.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // OPTIONS 허용

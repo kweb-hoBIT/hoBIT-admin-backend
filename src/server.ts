e1b2f268191571806384204cf02edfd6a3c7f0b2
BAE HYEONJIK <bhj4060@gmail.com>
2025-02-26 22:17:42 +0900

    FEAT: cors 추가

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,8 @@ const corsOptions = {
     env.CLIENT_URL1,
     env.CLIENT_URL2,
     "https://admin.hobit.kr",
-    "https://hobit-admin-frontend.vercel.app"
+    "https://hobit-admin-frontend.vercel.app",
+    /^https:\/\/.*\.vercel\.app$/
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // OPTIONS 허용

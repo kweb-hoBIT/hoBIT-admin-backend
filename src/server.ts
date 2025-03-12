0ae6d628615a613d5065955303b35fa084baef13
BaeHyeonJik <bhj4060@gmail.com>
2025-03-12 11:33:21 +0900

    FIX: swagger 비활성화

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import swaggerUi from "swagger-ui-express";
-import { swaggerDocs } from "../config/swaggerConfig";
+// import swaggerUi from "swagger-ui-express";
+// import { swaggerDocs } from "../config/swaggerConfig";
 import env from "../config/env";
 import { initializeDatabase } from "../config/createDB";
 
@@ -24,7 +24,6 @@ const corsOptions = {
   origin: [
     env.CLIENT_URL1,
     env.CLIENT_URL2,
-    "https://api2.hobit.kr",
     "https://admin.hobit.kr",
     /^https:\/\/.*\.vercel\.app$/,
   ],
@@ -54,8 +53,8 @@ app.get("/", (_req, res) => {
   res.send("API Running");
 });
 
-//Swagger 라우트
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// //Swagger 라우트
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // 라우트 설정
 app.use("/api", authRoutes);

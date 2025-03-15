b9c909ff3d8d4b0017f50a4f7b49e14adda9682a
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-15 20:21:32 +0900

    DELETE: auth 삭제

--- src/routes/api/auth/authIndex.ts
+++ src/routes/api/auth/authIndex.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
-import authRoutes from './auth';
 import loginRoutes from './login';
 import logoutRoutes from './logout';
 import tokenRefreshRoutes from './tokenRefresh';
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
 router.use('/auth', loginRoutes);
 router.use('/auth', logoutRoutes);
 router.use('/auth', tokenRefreshRoutes);

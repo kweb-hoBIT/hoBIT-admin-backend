ac2a89e7a1517d2c4eb6ae2dd0cbd171ad309a44
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-16 12:17:35 +0900

    DELETE: 삭제

--- src/models/RelatedFAQ.ts
+++
@@ -1,9 +0,0 @@
-export type RelatedFAQ = {
-  id: number;
-  faq_id: number;
-  related_faqs: Record<string, any>;
-  created_by: number | null;
-  updated_by: number | null;
-};
-
-export default RelatedFAQ;

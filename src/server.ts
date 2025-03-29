cf136d93595ce3759ffe0254689123c992200ba5
BaeHyeonJik <bhj4060@gmail.com>
2025-03-29 17:45:08 +0900

    FIX: env 추가

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
+import env from "../config/env";
 
-const port = process.env.PORT || 5001;
+const port = env.PORT || 5001;
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
 export default server;

fd4016c92ae36c1876fada0934c2f81c1eecb97e
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-30 02:56:10 +0900

    REFACTOR: console.log 변경

--- src/app.ts
+++ src/app.ts
@@ -69,7 +69,7 @@ app.use("/api", swaggerRoutes);
 
 
 cron.schedule('0 0 * * *', async () => {
-  console.log('Running getUnmatchedQuestion at midnight');
+  console.log('Running getUnmatchedQuestion');
   try {
     await getUnmatchedQuestion();
   } catch (err) {

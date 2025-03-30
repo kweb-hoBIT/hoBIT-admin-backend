689a527e635addebecc77bde7d7162f1b0b2b221
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-30 22:47:34 +0900

    REFACTOR: 시간변경

--- src/app.ts
+++ src/app.ts
@@ -68,7 +68,7 @@ app.use("/api", translateRoutes);
 app.use("/api", swaggerRoutes);
 
 
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('0 23 * * *', async () => {
   console.log('Running getUnmatchedQuestion');
   try {
     await getUnmatchedQuestion();

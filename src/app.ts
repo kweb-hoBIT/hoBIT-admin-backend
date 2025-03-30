b100f1108feac171e726c8b5a399cc1f561344f0
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-31 01:43:50 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -68,7 +68,7 @@ app.use("/api", translateRoutes);
 app.use("/api", swaggerRoutes);
 
 
-cron.schedule('43 1 * * *', async () => {
+cron.schedule('46 1 * * *', async () => {
   console.log('Running getUnmatchedQuestion');
   try {
     await getUnmatchedQuestion();
